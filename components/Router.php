196e8061abff2f9a999f2dc4a1389e8f8621b9fe
avenirs05 <avenirs05@gmail.com>
2017-07-17 17:47:00 +0300

    first

---
+++ components/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Description of Router
+ *
+ */
+
+class Router 
+{
+    
+}
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Front Controller
+
+
+// 1. Общие настройки
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// 2. Подключение файлов системы
+
+
+// 3. Установка соединения с БД
+
+
+// 4. Вызов Router
+
